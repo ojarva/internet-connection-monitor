1ea605e5390ac4c6c31e173d0f39b1ce5c99ae54
Olli Jarva <olli.jarva@futurice.com>
2016-02-29 21:46:39 +0200

    Initial commit

---
+++ src/measure_bandwidth.py
@@ -0,0 +1,142 @@
+from influxdb import InfluxDBClient
+from local_settings import SPEED_TEST_HOST, SPEED_TEST_PATH, BANDWIDTH_INTERVAL, FULL_BANDWIDTH_SIZES, FULL_BANDWIDTH_RATIO, BANDWIDTH_SIZES
+from setproctitle import setproctitle
+import datetime
+import httplib
+import json
+import redis
+import time
+
+
+assert isinstance(SPEED_TEST_HOST, str)
+assert isinstance(SPEED_TEST_PATH, str)
+assert isinstance(BANDWIDTH_INTERVAL, (int, float))
+assert BANDWIDTH_INTERVAL > 5
+assert isinstance(FULL_BANDWIDTH_RATIO, int)
+assert FULL_BANDWIDTH_RATIO > 0
+assert isinstance(BANDWIDTH_SIZES, (tuple, list))
+assert isinstance(FULL_BANDWIDTH_SIZES, (tuple, list))
+
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime.datetime):
+            return o.isoformat()
+
+        return json.JSONEncoder.default(self, o)
+
+
+class InternetConnectionSpeed(object):
+    def __init__(self):
+        self.redis = redis.StrictRedis()
+        self.influx = InfluxDBClient("localhost", 8086, "root", "root", "home")
+
+    @classmethod
+    def get_random_data(cls, size):
+        f = open("/dev/urandom")
+        remaining_size = size
+        data = ""
+        while remaining_size > 0:
+            data += f.read(remaining_size)
+            remaining_size = size - len(data)
+        return data
+
+    def measure_download(self, size):
+        start = time.time()
+        conn = httplib.HTTPConnection(SPEED_TEST_HOST)
+        conn.connect()
+        connected = time.time()
+        conn.request('GET', "/{path}?size={size}".format(path=SPEED_TEST_PATH, size=size))
+        request_time = time.time()
+        resp = conn.getresponse()
+        response_time = time.time()
+        response_content = resp.read()
+        size = len(response_content)
+        transferred = time.time()
+        conn.close()
+
+        gen_time = float(resp.getheader("X-gen-duration"))
+        transfer_time = transferred - response_time - gen_time
+
+        data = {
+            "fields": {
+                "connection_opened": round(connected - start, 8),
+                "request_sent": round(request_time - connected, 8),
+                "data_transfer": round(transfer_time, 8),
+                "size_in_bytes": size,
+                "server_generation": round(gen_time, 8),
+                "speed_mbits": round((float(size) / (transfer_time) * 8 / 1024 / 1024), 4),
+            },
+            "tags": {
+                "direction": "download",
+                "size": size,
+            },
+            "measurement": "internet_speed",
+            "time": datetime.datetime.utcnow().isoformat() + "Z",
+        }
+
+        return data
+
+    def measure_upload(self, size):
+        upload_data = self.get_random_data(size)
+        size = len(upload_data)
+        start = time.time()
+        conn = httplib.HTTPConnection(SPEED_TEST_HOST)
+        conn.connect()
+        connected = time.time()
+        conn.request('POST', "/" + SPEED_TEST_PATH, upload_data)
+        request_time = time.time()
+        resp = conn.getresponse()
+        resp.read()
+        transfer_time = time.time()
+        conn.close()
+
+        data = {
+            "fields": {
+                "connection_opened": round(connected - start, 8),
+                "data_transfer": round(request_time - connected, 8),
+                "response_received": round(transfer_time - request_time, 8),
+                "size_in_bytes": size,
+                "speed_mbits": round((float(size) / (request_time - connected) * 8 / 1024 / 1024), 4),
+            },
+            "tags": {
+                "direction": "upload",
+                "size": size,
+            },
+            "measurement": "internet_speed",
+            "time": datetime.datetime.utcnow().isoformat() + "Z",
+        }
+        return data
+
+    def fetch_once(self, sizes):
+        measurements = []
+        for size in sizes:
+            measurements.append(self.measure_download(size))
+            measurements.append(self.measure_upload(size))
+        self.redis.publish("influx-update-pubsub", json.dumps(measurements, cls=DateTimeEncoder))
+        self.influx.write_points(measurements)
+
+    def run(self):
+        last_fetch_at = time.time()
+        i = 0
+        while True:
+            i = i % FULL_BANDWIDTH_RATIO
+            if i == 0:
+                sizes = FULL_BANDWIDTH_SIZES
+            else:
+                sizes = BANDWIDTH_SIZES
+            self.fetch_once(sizes)
+            sleep_time = max(BANDWIDTH_INTERVAL / 4, BANDWIDTH_INTERVAL - (time.time() - last_fetch_at))
+            last_fetch_at = time.time()
+            print "Sleeping for %s" % sleep_time
+            time.sleep(sleep_time)
+            i += 1
+
+
+def main():
+    icp = InternetConnectionSpeed()
+    icp.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/measure_dns.py
@@ -0,0 +1,107 @@
+from influxdb import InfluxDBClient
+from local_settings import NAMESERVERS, VALID_DESTINATIONS, INVALID_DESTINATIONS, VALID_RANDOM_DESTINATIONS, INVALID_RANDOM_DESTINATIONS, DNS_INTERVAL
+import datetime
+import dns.resolver
+import json
+import redis
+import time
+import uuid
+
+
+assert isinstance(NAMESERVERS, (list, tuple))
+assert isinstance(VALID_DESTINATIONS, (list, tuple))
+assert isinstance(INVALID_DESTINATIONS, (list, tuple))
+assert isinstance(VALID_RANDOM_DESTINATIONS, (list, tuple))
+assert isinstance(INVALID_RANDOM_DESTINATIONS, (list, tuple))
+assert isinstance(DNS_INTERVAL, (int, float))
+
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime.datetime):
+            return o.isoformat()
+
+        return json.JSONEncoder.default(self, o)
+
+
+def time_method(func):
+    def inner(*args, **kwargs):
+        start_time = time.time()
+        func(*args, **kwargs)
+        end_time = time.time()
+        return round(float(end_time - start_time), 4)
+    return inner
+
+
+class DnsSpeed(object):
+    def __init__(self):
+        self.redis = redis.StrictRedis()
+        self.influx = InfluxDBClient("localhost", 8086, "root", "root", "home")
+        self.resolver = dns.resolver.Resolver()
+
+    @time_method
+    def resolve_dns(self, hostname):
+        try:
+            self.resolver.query(hostname, "A")
+        except dns.resolver.NXDOMAIN:
+            pass
+
+    def test(self, hostname):
+        data = {}
+        data["first_try"] = self.resolve_dns(hostname)
+        data["second_try"] = self.resolve_dns(hostname)
+        return data
+
+    def write_data(self, nameserver, destination, data, extra_tags=None):
+        tags = {
+            "nameserver": nameserver,
+            "destination": destination,
+        }
+        if extra_tags is not None:
+            tags.update(extra_tags)
+        influx_data = [{
+            "measurement": "dns_speed",
+            "tags": tags,
+            "time": datetime.datetime.utcnow().isoformat() + "Z",
+            "fields": data,
+        }]
+        return influx_data
+
+    def fetch_once(self):
+        measurements = []
+        for nameserver in NAMESERVERS:
+            self.resolver.nameservers = [nameserver]
+            for destination in VALID_DESTINATIONS:
+                data = self.test(destination)
+                measurements.append(self.write_data(nameserver, destination, data, {"type": "valid", "class": "predefined"}))
+            for destination in INVALID_DESTINATIONS:
+                data = self.test(destination)
+                measurements.append(self.write_data(nameserver, destination, data, {"type": "invalid", "class": "predefined"}))
+            for destination in VALID_RANDOM_DESTINATIONS:
+                generated_destination = str(uuid.uuid4()) + "." + destination
+                data = self.test(generated_destination)
+                measurements.append(self.write_data(nameserver, destination, data, {"type": "valid", "class": "random"}))
+            for destination in INVALID_RANDOM_DESTINATIONS:
+                generated_destination = str(uuid.uuid4()) + "." + destination
+                data = self.test(generated_destination)
+                measurements.append(self.write_data(nameserver, destination, data, {"type": "invalid", "class": "random"}))
+        self.redis.publish("influx-update-pubsub", json.dumps(measurements, cls=DateTimeEncoder))
+        self.influx.write_points(measurements)
+
+    def run(self):
+        last_fetch_at = time.time()
+        while True:
+            self.fetch_once()
+            sleep_time = max(DNS_INTERVAL / 4, DNS_INTERVAL - (time.time() - last_fetch_at))
+            last_fetch_at = time.time()
+            print "Sleeping for %s" % sleep_time
+            time.sleep(sleep_time)
+
+
+def main():
+    ds = DnsSpeed()
+    ds.run()
+
+
+if __name__ == '__main__':
+    main()
